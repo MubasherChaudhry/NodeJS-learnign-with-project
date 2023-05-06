d5a836cae5ba5408c28942bf37a14dc745f3d51e
Mubasher <mobi_20_11@yahoo.com>
2023-05-07 00:05:08 +0200

    overviewpage updated-1

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const fs = require ('fs')
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+//#+#+#+#+#+#+#+#++##+#+#++#+#
+//FILES
+
+/* 
+//This is an Example /Exercise of Blocking/ synchronous way
 
 const textIn=fs.readFileSync('./txt/input.txt', 'utf-8')
 console.log(textIn);
@@ -7,3 +15,96 @@ const textOut= `This is what we know about to avocado: ${textIn}. /n Created on 
 
 fs.writeFileSync('./txt/output.txt',textOut)
 console.log('File written!');
+
+//This is an Example /Exercise of Non-Blocking /asynchronous way
+
+fs.readFile(`./txt/start.txt`, "utf-8", (err, data1) => {
+  if (err) return console.log('There is a freaking ERROR!!!!');
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
+      fs.writeFile('./txt/final.txt',`${data2}\n${data3}`, 'utf-8', err =>{
+        console.log('Freaking file has been written');
+      })
+
+    });
+  });
+});
+
+console.log("will read File");
+
+ */
+
+//#+#+#+#+#+#+#+#++##+#+#++#+#
+//SERVERS
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const PORT = 5000;
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  //Overview Page
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, { "content-type": "text/html" });
+
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el));
+
+    console.log(cardsHtml);
+
+    res.end(tempOverview);
+
+    //Product Page
+  } else if (pathName === "/product") {
+    res.end("This is the product");
+
+    //Api Page
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(data);
+
+    //Not Found Page
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "My-own-header": "Hello MoBi",
+    });
+    res.end("<h1>page not found<h1/>");
+  }
+
+  // res.end("Hello from the Server Siiiiiiiiiiide!!!!!!!!!!!!!");
+});
+
+server.listen(PORT, "localhost", () => {
+  console.log(`Server is running for life on port ${PORT}`);
+});
