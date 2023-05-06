9abc4b1396103df2c05853e6763703e5c2df3b30
Mubasher <mobi_20_11@yahoo.com>
2023-05-06 15:32:33 +0200

    commit on for starting structure

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require ('fs')
+
+const textIn=fs.readFileSync('./txt/input.txt', 'utf-8')
+console.log(textIn);
+
+const textOut= `This is what we know about to avocado: ${textIn}. /n Created on ${Date.now()}`;
+
+fs.writeFileSync('./txt/output.txt',textOut)
+console.log('File written!');
